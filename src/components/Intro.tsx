d90d5888a4c3e38b33fef0dfe25fe37979898a07
matias claro <mclaro@ceinegocios.cl>
2020-06-28 08:51:19 -0400

    components

---
+++ src/components/Intro.tsx
@@ -0,0 +1,17 @@
+import * as Reacr from 'react'
+
+export default class Intro extends React.Component {    
+    public render() {
+        return (
+            <p>Edit <code>src/App.tsx</code> and save to reload.</p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+        )
+    }
+}
